c3f6e8a5edf7bee5c38f06823b339ae3b8bf4f86
jtheis85 <jatheis2@gmail.com>
2015-09-01 21:21:54 -0700

    Initial Commit
    Initial Commit

---
+++ ToggleDistrictSnapping/ModInfo.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace ToggleDistrictSnapping
+{
+    public class ModInfo : IUserMod
+    {
+        public string Name        { get { return "Toggle District Snapping"; } }
+        public string Description { get { return "Allows toggling of snapping to roads on and off while painting districts."; } }
+    }
+}
